54f2bf2270ad40e75bc0a46b277bcc770e00722f
James Ramasta <jamesadrianramasta@gmail.com>
2022-04-23 07:02:45 +0800

    update commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const container = document.querySelector(".container"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    login = document.querySelector(".login-link"),
+    btnregcancel = document.querySelector(".btnregcancel"),
+    btnlogin = document.querySelector(".btnlog"),
+    btnreg = document.querySelector(".btnreg");
+
+  const homepage = document.querySelector(".homepage");
+
+  //   js code to show/hide password and change icon
+  pwShowHide.forEach((eyeIcon) => {
+    eyeIcon.addEventListener("click", () => {
+      pwFields.forEach((pwField) => {
+        if (pwField.type === "password") {
+          pwField.type = "text";
+
+          pwShowHide.forEach((icon) => {
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+          });
+        } else {
+          pwField.type = "password";
+
+          pwShowHide.forEach((icon) => {
+            icon.classList.replace("uil-eye", "uil-eye-slash");
+          });
+        }
+      });
+    });
+  });
+
+  //js code to appear signup and login form
+  signUp.addEventListener("click", () => {
+    container.classList.add("active");
+  });
+  login.addEventListener("click", () => {
+    container.classList.remove("active");
+  });
+
+  //cancel button in register form
+  btnregcancel.addEventListener("click", () => {
+    container.classList.remove("active");
+  });
+
+   //cancel button in register form
+   btnreg.addEventListener("click", () => {
+    container.classList.remove("active");
+  });
+  
+ 
 }
-
-export default App;
