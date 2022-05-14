d94ee18ab2447206e7587950cf452bfb3035f94f
James Ramasta <jamesadrianramasta@gmail.com>
2022-05-14 08:13:06 +0800

    modified

--- src/App.js
+++ src/App.js
@@ -1,55 +1,79 @@
 import "./App.css";
 
+const btnStartElement = document.getElementById("btnStart");
+const btnBackElement = document.getElementById("btnBack");
+const btnAboutElement = document.getElementById("btnAbout");
+const btnBack2Element = document.getElementById("btnBack2");
+const btnSignup = document.getElementById("btnSignup");
+//const btnReg = document.getElementById("btnReg");
+const btnBack2Login = document.getElementById("btnbackLogin");
+//const btnLogin = document.getElementById("btnLogin");
+
+
+
+const gameStart = document.getElementById("gameStart");
+const container = document.getElementById("container");
+const aboutUs = document.getElementById("about");
+const signUp = document.getElementById("form-signup");
+
+
+
 export default function App() {
-  const container = document.querySelector(".container"),
-    pwShowHide = document.querySelectorAll(".showHidePw"),
-    pwFields = document.querySelectorAll(".password"),
-    signUp = document.querySelector(".signup-link"),
-    login = document.querySelector(".login-link"),
-    btnregcancel = document.querySelector(".btnregcancel"),
-    btnlogin = document.querySelector(".btnlog"),
-    btnreg = document.querySelector(".btnreg");
+  /*Start the game*/
+  btnStartElement.addEventListener("click", startGame);
+  function startGame() {
+    btnStartElement.classList.add("hide");
+    gameStart.classList.remove("hide");
+    container.classList.add("hide");
+  }
 
-  const homepage = document.querySelector(".homepage");
+  btnBackElement.addEventListener("click", backHome);
+  function backHome() {
+    btnStartElement.classList.remove("hide");
+    gameStart.classList.add("hide");
+    container.classList.remove("hide");
+  }
 
-  //   js code to show/hide password and change icon
-  pwShowHide.forEach((eyeIcon) => {
-    eyeIcon.addEventListener("click", () => {
-      pwFields.forEach((pwField) => {
-        if (pwField.type === "password") {
-          pwField.type = "text";
+  btnAboutElement.addEventListener("click", about);
+  function about() {
+    btnStartElement.classList.add("hide");
+    gameStart.classList.add("hide");
+    container.classList.add("hide");
+    aboutUs.classList.remove("hide");
+  }
 
-          pwShowHide.forEach((icon) => {
-            icon.classList.replace("uil-eye-slash", "uil-eye");
-          });
-        } else {
-          pwField.type = "password";
+  btnBack2Element.addEventListener("click", backstartGame);
+  function backstartGame() {
+    btnStartElement.classList.add("hide");
+    gameStart.classList.remove("hide");
+    container.classList.add("hide");
+    aboutUs.classList.add("hide");
+  }
 
-          pwShowHide.forEach((icon) => {
-            icon.classList.replace("uil-eye", "uil-eye-slash");
-          });
-        }
-      });
-    });
-  });
+  btnSignup.addEventListener("click", SignUp);
+  function SignUp() {
+    btnStartElement.classList.add("hide");
+    gameStart.classList.add("hide");
+    container.classList.add("hide");
+    aboutUs.classList.add("hide");
+    signUp.classList.remove("hide");
+  }
 
-  //js code to appear signup and login form
-  signUp.addEventListener("click", () => {
-    container.classList.add("active");
-  });
-  login.addEventListener("click", () => {
-    container.classList.remove("active");
-  });
 
-  //cancel button in register form
-  btnregcancel.addEventListener("click", () => {
-    container.classList.remove("active");
-  });
+  btnBack2Login.addEventListener("click", backLogin);
+  function backLogin() {
+    btnStartElement.classList.add("hide");
+    gameStart.classList.remove("hide");
+    container.classList.add("hide");
+    aboutUs.classList.add("hide");
+    signUp.classList.add("hide");
+  }
 
-   //cancel button in register form
-   btnreg.addEventListener("click", () => {
-    container.classList.remove("active");
-  });
+
   
+
+
+
  
+
 }
